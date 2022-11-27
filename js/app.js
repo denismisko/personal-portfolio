fcea2029735f4601aee7f81d848bef0af545c1f8
Denis Miškolci <denis.miskolci@kosickaakademia.sk>
2022-11-27 16:57:04 +0100

    add loader , mobile menu, and some other changes<3

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
+// SCROLL ANIMATIONS
+
 const observer = new IntersectionObserver((entries) => {
 
     entries.forEach((entry) => {
 
         if (entry.isIntersecting) {
 
-            entry.target.classList.add('show');
+            entry.target.classList.add('show')
             
         }
 
         else {
-            entry.target.classList.remove('show');
+
+            entry.target.classList.remove('show')
+
         }
 
     });
@@ -18,4 +22,43 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
-hiddenElements.forEach((el) => observer.observe(el)); 
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+// LOADER
+
+var loader = document.querySelector(".loader");
+
+window.addEventListener("load" , vanish);
+
+function vanish() {
+
+    loader.classList.add("disppear")
+    
+};
+
+
+// MOBILE NAVIGATION OTVORENIE
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click" , mobileMenu);
+
+function mobileMenu() {
+
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+    
+}
+
+// MOBILE NAVIGATION  ZATVORENIE
+
+const navLink = document.querySelectorAll(".menu");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
